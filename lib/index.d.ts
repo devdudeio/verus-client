4db3a5296b496ca6f33c84720124794e5d17a6ce
dude <you@example.com>
2021-12-27 00:50:46 +0100

    addded getBalance example

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,21 @@
 import { AxiosInstance } from 'axios';
 interface VerusClientInstance {
-    client: AxiosInstance;
+    /**
+     * @description the total amount in the wallet
+     * @example
+     * // the total amount in the wallet
+     * .getBalance();
+     * @example
+     * // the total amount in the wallet at least 5 blocks confirmed
+     * .getBalance('*', 5);
+     * @returns {Promise<number>} total number of coins available in the wallet
+     */
+    getBalance(account?: string, minconf?: number, includeWatchonly?: boolean): Promise<number>;
+    _axiosInstance: AxiosInstance;
 }
 declare class VerusClient implements VerusClientInstance {
-    client: AxiosInstance;
-    constructor(rpcBaseUrl: string, rpcUsername: string, rpcPassword: any);
+    _axiosInstance: AxiosInstance;
+    constructor(rpcBaseUrl: string, rpcUsername: string, rpcPassword: string);
+    getBalance(account: any, minconf: any, includeWatchonly: any): Promise<any>;
 }
 export default VerusClient;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,48 @@
 import axios from "axios";
-import { AxiosInstance } from 'axios'
+import { AxiosInstance, AxiosResponse } from 'axios'
 
 interface VerusClientInstance {
-    client: AxiosInstance
+    
+    /**
+     * @description the total amount in the wallet
+     * @example
+     * // the total amount in the wallet
+     * .getBalance();
+     * @example
+     * // the total amount in the wallet at least 5 blocks confirmed
+     * .getBalance('*', 5);
+     * @returns {Promise<number>} total number of coins available in the wallet
+     */
+    getBalance(account?: string, minconf?: number, includeWatchonly?: boolean): Promise<number>
+    _axiosInstance: AxiosInstance
 }
 
 class VerusClient implements VerusClientInstance {
-    client: AxiosInstance;
-    constructor(rpcBaseUrl: string, rpcUsername: string, rpcPassword) {
-        this.client = axios.create({
+    _axiosInstance: AxiosInstance;
+       
+    constructor(rpcBaseUrl: string, rpcUsername: string, rpcPassword: string){
+        this._axiosInstance = axios.create({
             baseURL: rpcBaseUrl,
             auth: {
                 username: rpcUsername,
                 password: rpcPassword,
             }
-        })
+        })     
+      }
+    async getBalance(account, minconf, includeWatchonly){
+          try {
+            return await this._axiosInstance.post('', {
+                method: 'getbalance',
+                params: [],
+            })
+            .then(response => parseFloat(response.data.result))
+            .catch(err => {throw new Error(err)})
+        } catch (err) {
+            return err;
+        }
     }
 }
+
+
 
 export default VerusClient
